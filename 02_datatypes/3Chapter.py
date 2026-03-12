b580d4090a8d99c7cc6e6bafd11a0fe98b294397
itssunnyfunny <sunnyfunny4062@gmail.com>
2026-03-12 15:40:28 +0530

    Integers in Python

---
+++ 02_datatypes/3Chapter.py
@@ -0,0 +1,35 @@
+# Interger 
+black_tea_grams = 14
+ginger_grams= 3
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of base tea is {total_grams}")
+
+remaining_tea = black_tea_grams - ginger_grams
+print(f"TOtal grams of reamining tea is {remaining_tea}")
+
+milk_litres = 7
+servings = 4
+milk_per_serving =milk_litres / servings
+
+print(f"Milk per serving is {milk_per_serving}")
+
+total_tea_bags = 7 
+pots = 4
+bags_pre_pot = total_tea_bags // pots
+print(f"Whole tea bags per pot: {bags_pre_pot}")
+
+total_cadamon_pods = 10 
+pods_per_cup = 3
+lefover_pods = total_cadamon_pods % pods_per_cup
+print(f"left over C pods {lefover_pods}")
+
+
+base_flovor_strenght = 2
+scale_factor = 3
+powerful_falvour = base_flovor_strenght ** scale_factor
+print(f"Scale flovour strength {powerful_falvour}")
+
+total_tea_laves_harversted = 1_000_000_000
+
+print(f"crore : {total_tea_laves_harversted}")

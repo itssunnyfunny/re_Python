1664093a4cf7d5e949aa301b51a4d2047545353f
itssunnyfunny <sunnyfunny4062@gmail.com>
2026-03-12 15:16:04 +0530

    datatypes in python:

---
+++ 02_datatypes/1Chapter.py
@@ -0,0 +1,9 @@
+sugar_amount = 100
+print(f"Initial sugar: {sugar_amount}")
+
+sugar_amount = 200
+
+print(f"second sugar:{sugar_amount} ")
+
+print(f"ID of 100: {id(100)}")
+print(f"ID of 200: {id(200)}")

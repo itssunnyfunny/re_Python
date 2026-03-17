301c60f744806f76eef4feba564b0b538962d0ba
itssunnyfunny <sunnyfunny4062@gmail.com>
2026-03-17 15:31:08 +0530

    funtion and call it in python

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,7 @@
+def print_order(name, chai_type):
+    print(f"Hello {name} your chai type is {chai_type}")
+    
+
+print_order("Aman", "masala chai")
+print_order("Shani", "ginger chai")
+print_order("Priya", "black chai")
---
+++ 05_functions/02_complex.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+    print("Fetching sales data")
+
+def filter_valid_sales():
+    print("Filtering valid sales")
+
+def summarize_data():
+    print("Summarizing data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+    print("Report is ready")
+
+generate_report()

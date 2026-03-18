70e77ab133d68065717bdeafa82943bc21d8b87e
itssunnyfunny <sunnyfunny4062@gmail.com>
2026-03-18 14:37:03 +0530

    imports, modules and init files in python

---
+++ 06_chai_business/main.py
@@ -0,0 +1,13 @@
+# import recipes.flavors 
+
+# print(recipes.flavors.elachai_chai())
+
+from recipes.flavors import elachai_chai, ginger_chai
+
+print(elachai_chai())
+print(ginger_chai())
+
+# from ..recipes.flavors import black_chai
+
+# print(black_chai())
+
---
+++ 06_chai_business/recipes/flavors.py
@@ -0,0 +1,8 @@
+def elachai_chai():
+    return "Elachai chai"
+
+def ginger_chai():
+    return "Ginger chai"
+
+def black_chai():
+    return "Black chai"
